ebbada5f5cab6ec35443d62cfddcdde20d1228e1
nawinkhatiwada <nabin@Nabins-MacBook-Pro.local>
2019-06-15 20:14:28 +0545

    unused file removed

--- app/src/main/java/com/techsales/taskmanager/utils/Config.java
+++
@@ -1,6 +0,0 @@
-package com.techsales.taskmanager.utils;
-
-public class Config {
-    public static final String NOTES_TABLE_NAME = "notes";
-    public static final String DATABASE_NAME = "notes.db";
-}
